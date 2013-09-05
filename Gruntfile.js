0dfe3bcb21aca0460a8f077b7850ed71c809e80b
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-09-05 15:41:00 +0200

    initial import

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+		jshint: {
+			src: [
+				'Gruntfile.js',
+				'tasks/*.js'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+		geojsonhint : {
+			good: [
+				'node_modules/geojsonhint/test/data/good/*.geojson'
+			],
+			bad: [
+				'node_modules/geojsonhint/test/data/bad/*.geojson'
+			]
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadTasks('tasks/');
+
+	grunt.registerTask('default', ['jshint']);
+}
---
+++ tasks/geojsonhint.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var geojsonhint = require('geojsonhint');
+
+module.exports = function (grunt) {
+	grunt.registerMultiTask('geojsonhint', 'Lint geojson files.', function () {
+		var errorCount = 0;
+		var errors;
+
+		if (this.files !== null) {
+			this.filesSrc.forEach(function (filepath) {
+				errors = geojsonhint.hint(grunt.file.read(filepath));
+
+				if (errors.length && errors.length > 0) {
+					grunt.log.writeln('Hinting', filepath);
+					errors.forEach(function (error) {
+						grunt.log.writeln('['.red + 'L' + error.line + ']'.red + ':', error.message);
+					});
+					errorCount++;
+					grunt.log.writeln();
+				}
+			});
+			if (errorCount === 0) {
+				grunt.log.ok(this.files.length, ' files without errors');
+			} else {
+				return false;
+			}
+		}
+	});
+};
